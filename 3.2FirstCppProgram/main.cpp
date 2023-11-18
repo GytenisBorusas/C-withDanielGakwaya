883ddc6691fe087f668cc40bac75f5e118685e79
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-11-18 10:17:40 -0700

    first commit for comments sections

--- 3.2FirstCppProgram/main.cpp
+++ 3.2FirstCppProgram/main.cpp
@@ -1,6 +1,15 @@
+
 #include <iostream>
 
+
+
+
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2";
+
+    return 0;
+
+
 }
