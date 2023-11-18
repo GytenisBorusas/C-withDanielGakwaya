bc1e51e79cf54a714ba763e00421a13577c20bc5
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-11-18 12:11:18 -0700

    3.6 done

--- 3.6DataInputsAndOutputs/main.cpp
+++ 3.6DataInputsAndOutputs/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <string>
 
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+    //printing data
+    std::cout << "Hello C++20 " << std::endl;
+
+    /*
+    int age{21};
+    std::cout << "Age : " << age << std::endl;
+
+    std::cerr << "Error message : something is wrong" << std::endl;
+    std::clog << "Log message : Something happened " << std::endl;
+    */
+
+    /*
+    //
+    int age1;
+    std::string name;
+
+    std::cout << "Please type your name and age : " << std::endl;
+
+    
+    // std::cin >> name; 
+    // std::cin >> age1;
+    
+    std::cin >> name >> age1; //Getting data in one line instead of 2 (like above)
+
+    std::cout << "Hello " << name << " you are " << age1 << " years old!" << std::endl;
+
+    */
+
+
+    // Data with spaces
+    std::string full_name;
+    int age3;
+
+    std::cout << "please type in your full name and age " << std::endl;
+
+    std::getline(std::cin,full_name);
+    std::cin >> age3;
+
+    std::cout << "Hello " << full_name << " you are " << age3 << " years old!" << std::endl;
+
+    return 0;
 }
