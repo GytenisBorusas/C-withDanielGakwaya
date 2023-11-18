5bbcd3fff1897bbf402ba84879af9fc7d8ba69ed
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-11-18 11:25:37 -0700

    done with 3.5 lecture

--- 3.5StatementsAndFunctions/main.cpp
+++ 3.5StatementsAndFunctions/main.cpp
@@ -12,7 +12,7 @@ int main(){
 
     std::cout << "First number : " << first_number << std::endl; // 13
     std::cout << "Second number : " << second_number << std::endl; // 7
-    /*
+    
     int sum = first_number + second_number;
     std::cout << "Sum : " << sum << std::endl; // 20
 
@@ -20,10 +20,10 @@ int main(){
     std::cout << "Sum : " << sum << std::endl; // 32
 
     sum = addNumbers(30,54);
-    std::cout << "Sum : " << sum << std::endl;
-
-    std::cout << "Sum : " << addNumbers(3,42) << std::endl;
-    */
+    std::cout << "Sum : " << sum << std::endl; //84
+    
+    std::cout << "Sum : " << addNumbers(3,42) << std::endl; //45
+    
 
     return 0;
 }
