e40f1406cc68d4c34da2c0381c73897784860617
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-11-19 11:49:16 -0700

    done with 5.2

--- Chapter 5 - Operations On Data/5.2BasicOperations/main.cpp
+++ Chapter 5 - Operations On Data/5.2BasicOperations/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+    
+    int number1{2};
+    int number2{7}; 
+
+    //addition
+    int result = number1 + number2;
+    std::cout << "result : " << result << std::endl;
+
+    //subtraction
+    result = number2 - number1;
+    std::cout << "result : " << result << std::endl;
+
+    result = number1 - number2;
+    std::cout << "result : " << result << std::endl;
+
+    //Multiplication
+    result = number1 * number2;
+    std::cout << "result : " << result << std::endl;
+
+
+    //Division
+    result = number2 / number1;
+    std::cout << "result : " << result << std::endl;
+
+
+    //Modulus
+    result = number2 % number1; // 7 % 2
+    std::cout << "result : " << result << std::endl; // 1
+
+    result = 31 % 10;
+    std::cout << "result : " << result << std::endl; // 1
+     return 0;
 }
