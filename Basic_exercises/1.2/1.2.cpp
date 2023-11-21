507a6d60ab4231c7203ca2afa7034e358b8b794b
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-11-21 15:35:40 -0700

    start of basic exercises

---
+++ Basic_exercises/1.2/1.2.cpp
@@ -0,0 +1,14 @@
+/*
+
+1. Write a program in C++ to print a welcome text in a separate line.
+
+*/
+
+
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
