7468dfb5e8d471465e5cce4d4292928d23de3bcc
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-11-22 10:23:47 -0700

    done with 1.8

--- Basic_exercises/1.8/1.8.cpp
+++ Basic_exercises/1.8/1.8.cpp
@@ -14,6 +14,8 @@ Product is : 0
 
 */
 
+
+
 #include <iostream>
 #include <limits>
 
@@ -23,18 +25,30 @@ Product is : 0
 
 int main(){
 
-    int number1 = -2147483648;
-    int number2 = number1+1;
+    int number1 = std::numeric_limits<int>::min();
+    int number2 = number1 + 1;
     int product1 = number2 - number1;
+
+    
+
 
     std::cout << "Overflow the integer range and set in minimum range : " << std::numeric_limits<int>::min() << std::endl;
     std::cout << "Increasing from its minimum range : " << number2 << std::endl;
     // std::cout << std::fixed << std::setprecision(1); // This sets the precision for floating point output
     std::cout << "Product is :" << product1 << std::endl;
     
+    int number3 = std::numeric_limits<int>::max();
+    std::cout << "Underflow the range and set in maximum range : " << std::numeric_limits<int>::max() << std::endl;
+    int number4 = number3 - 1;
+    std::cout << "Decreasing from its maximum range : " << number4 << std::endl;
+    int product2 = number3 - number4;
+    std::cout << "Product is :" << product1 << std::endl;
 
 
 
 
     return 0;
 }
+
+
+
