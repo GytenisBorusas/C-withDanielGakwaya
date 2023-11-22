d9ed8ec297d94ebe68223351d3a19ce358647689
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-11-22 11:12:27 -0700

    1.10 needs to come back to it

--- Basic_exercises/1.10/1.10.cpp
+++ Basic_exercises/1.10/1.10.cpp
@@ -1,46 +1,47 @@
 /*
 
-5. Write a program in C++ to check the upper and lower limits of integer.
-Expected Output:
-Check the upper and lower limits of integer :
---------------------------------------------------
-The maximum limit of int data type : 2147483647
-The minimum limit of int data type : -2147483648
-The maximum limit of unsigned int data type : 4294967295
-The maximum limit of long long data type : 9223372036854775807
-The minimum limit of long long data type : -9223372036854775808
-The maximum limit of unsigned long long data type : 18446744073709551615
-The Bits contain in char data type : 8
-The maximum limit of char data type : 127
-The minimum limit of char data type : -128
-The maximum limit of signed char data type : 127
-The minimum limit of signed char data type : -128
-The maximum limit of unsigned char data type : 255
-The minimum limit of short data type : -32768
-The maximum limit of short data type : 32767
-The maximum limit of unsigned short data type : 65535
+https://learn.saylor.org/mod/book/view.php?id=33554
+
+10. Write a program in C++ to formatting the output.
+Sample Output:
+Formatting the output :
+----------------------------
+The value of pi : 3.1416
+The value of pi 4 decimal place of total width 8 : | 3.1416|
+The value of pi 4 decimal place of total width 10 : | 3.1416|
+The value of pi 4 decimal place of total width 8 : |--3.1416|
+The value of pi 4 decimal place of total width 10 : |----3.1416|
+The value of pi in scientific format is : 3.1416e+00
+Status
+in number : 0
+Status in alphabet : false
 
 */
 
 #include <iostream>
-#include <limits>
+#include <iomanip>
 
-
+float pi = 3.1416;
 
 int main(){
 
-    std::cout << "The minumum limit of int data type : " << std::numeric_limits<int>::min() << std::endl;
-    std::cout << "The maximum limit of int data type : " << std::numeric_limits<int>::max() << std::endl;
-    std::cout << "The maximum limit of unsigned int data type : " << std::numeric_limits<unsigned int>::max() << std::endl;
-    std::cout << "The minumum limit of long long data type : " << std::numeric_limits<long long>::min() << std::endl;
-    std::cout << "The maximum limit of long long data type : " << std::numeric_limits<long long>::max() << std::endl;
-    std::cout << "The maximum limit of unsigned long long data type : " << std::numeric_limits<unsigned long long>::max() << std::endl;
-    std::cout << "The minumum limit of char data type : " << static_cast<int>(std::numeric_limits<char>::min()) << std::endl;
-    std::cout << "The maximum limit of char data type : " << static_cast<int>(std::numeric_limits<char>::max()) << std::endl;
-    std::cout << "The maximum limit of unsigned char data type : " << static_cast<int>(std::numeric_limits<unsigned char>::max()) << std::endl;
-    std::cout << "The minumum limit of short data type : " << std::numeric_limits<short>::min() << std::endl;
-    std::cout << "The maximum limit of short data type : " << std::numeric_limits<short>::max() << std::endl;
-    std::cout << "The maximum limit of unsigned short data type : " << std::numeric_limits<unsigned short>::max() << std::endl;
+    int firstWidth{60};
+    int secondWidth{8};
+    int thirdWidth{10};
+
+    std::cout << "The value of pi : " << pi << std::endl;
+
+    std::cout << std::setw(firstWidth) <<  "The value of pi 4 decimal place of total width 8 : " << std::setw(secondWidth) << pi << std::endl;
+    std::cout << std::setw(firstWidth) <<  "The value of pi 4 decimal place of total width 10 : " << std::setw(thirdWidth) << pi << std::endl;
+
+    std::cout << std::setfill('-'); // The fill character
+    std::cout << std::setw(firstWidth) <<  "The value of pi 4 decimal place of total width 8 : " << std::setw(secondWidth) << pi << std::endl;
+    std::cout << std::setw(firstWidth) <<  "The value of pi 4 decimal place of total width 10 : " << std::setw(secondWidth) << pi << std::endl;
+
+    std::cout << std::scientific;
+    std::cout << std::setw(30) <<  "The value of pi in scientific format is : "<< pi << std::endl;
+    
+
 
     return 0;
 }
